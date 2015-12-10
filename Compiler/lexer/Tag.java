7ea67c1aed3aa09033779da5e253741325507a96
Everton Matos <matos.everton@gmail.com>
2015-12-10 21:31:20 -0200

    Alteração Final - Entrega

--- Compiler/lexer/Tag.java
+++ Compiler/lexer/Tag.java
@@ -3,10 +3,12 @@ package lexer;
 public class Tag {
 
    public final static int
-      AND     = 256,  BASIC = 257,  BREAK = 258,  DO   = 259, ELSE  = 260,
-      EQ      = 261,  FALSE = 262,  GE    = 263,  ID   = 264, IF    = 265,
-      INDEX   = 266,  LE    = 267,  MINUS = 268,  NE   = 269, NUM   = 270,
-      OR      = 271,  REAL  = 272,  TEMP  = 273,  TRUE = 274, WHILE = 275,
-      PROGRAM = 276,  BEGIN = 277,  END   = 278,  VAR  = 279, THEN  = 280,
-      ARRAY	  = 281,  OF    = 282;
+      AND     = 256,  	BASIC = 257,  BREAK = 258,  DO   = 259, 	ELSE  = 260,
+      EQ      = 261,  	FALSE = 262,  GE    = 263,  ID   = 264, 	IF    = 265,
+      INDEX   = 266,  	LE    = 267,  MINUS = 268,  NE   = 269, 	NUM   = 270,
+      OR      = 271,  	REAL  = 272,  TEMP  = 273,  TRUE = 274,		WHILE = 275,
+      PROGRAM = 276,  	BEGIN = 277,  END   = 278,  VAR  = 279, 	THEN  = 280,
+      ARRAY	  = 281,  	OF    = 282,  FOR   = 283,  TO   = 284, 	DOWNTO = 285,
+      CASE    = 286,  	REPEAT = 287, UNTIL = 288,  WRITELN = 289,  READLN = 290,
+      PROCEDURE = 291,  FUNCTION=292;
 }
