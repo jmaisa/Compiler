061ee1bc592ee1c082b2fcd6c64482c730eaa2f6
Everton Matos <matos.everton@gmail.com>
2015-12-10 21:31:41 -0200

    Alteração Final - Entrega

--- Compiler/symbols/Array.java
+++ Compiler/symbols/Array.java
@@ -2,9 +2,14 @@ package symbols;
 import lexer.*;
 public class Array extends Type {
    public Type of;                  // array *of* type
+ //  public Type array;
    public int size = 1;             // number of elements
    public Array(int sz, Type p) {
       super("[]", Tag.INDEX, sz*p.width); size = sz;  of = p;
    }
-   public String toString() { return "[" + size + "] " + of.toString(); }
+   
+   public String toString() { 
+    //   return array.toString() + "[" + size + "] " + of.toString(); 
+       return "[" + size + "] " + of.toString(); 
+   }
 }
